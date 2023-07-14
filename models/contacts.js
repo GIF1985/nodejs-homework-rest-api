859d00384495f93c9e38d4f01e9bc5b0b54d4c34
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-07-14 15:38:11 +0300

    hw02

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,64 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
 
-const listContacts = async () => {}
+const contactsPath = path.join(process.cwd(), "data", "contacts.json");
 
-const getContactById = async (contactId) => {}
+export async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error(error.message);
+    return [];
+  }
+}
 
-const removeContact = async (contactId) => {}
+export async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((item) => item.id === contactId);
+    return contact || null;
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+}
 
-const addContact = async (body) => {}
+export async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contactIndex = contacts.findIndex((item) => item.id === contactId);
+    if (contactIndex === -1) {
+      return null;
+    }
+    const [removedContact] = contacts.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removedContact;
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+}
 
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+export async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const newContact = {
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
 }
---
+++ routes/api/contacts-router.js
@@ -0,0 +1,70 @@
+import express from "express";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "../../models/contacts.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const contacts = await listContacts();
+    res.json(contacts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const contact = await getContactById(id);
+    if (contact) {
+      res.json(contact);
+    } else {
+      res.status(404).json({ message: "Contact not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400).json({ message: "Missing required fields" });
+    return;
+  }
+  try {
+    const newContact = await addContact(name, email, phone);
+    if (newContact) {
+      res.status(201).json(newContact);
+    } else {
+      res.status(500).json({ message: "Failed to add contact" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const removedContact = await removeContact(id);
+    if (removedContact) {
+      res.json({ message: "Contact deleted" });
+    } else {
+      res.status(404).json({ message: "Contact not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+export default router;
